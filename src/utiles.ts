41dc5478164574599658c7eff117a9bd7b462ef8
Luis Cabrera Benito <parzibyte@gmail.com>
2025-06-25 21:03:19 -0600

    Fecha para humanos al crear mensaje
    No mostrar fecha en formato YYYY-MM-DD, mostrar formateada entendible por humanos

--- src/utiles.ts
+++ src/utiles.ts
@@ -1,9 +1,10 @@
 import type { Cliente } from "./Clases";
 
 export const generarMensaje = (plantilla: string, firma: string, cliente: Cliente, fechaInicio: string, fechaFin: string): string => {
+    const formateadorFecha = new Intl.DateTimeFormat("es-MX", { dateStyle: 'medium', });
     return plantilla
         .replaceAll("{correoCliente}", cliente.correo)
         .replaceAll("{firma}", firma)
-        .replaceAll("{fechaInicio}", fechaInicio)
-        .replaceAll("{fechaFin}", fechaFin)
+        .replaceAll("{fechaInicio}", formateadorFecha.format(new Date(fechaInicio + "T00:00:00")))
+        .replaceAll("{fechaFin}", formateadorFecha.format(new Date(fechaFin + "T00:00:00")))
 }
