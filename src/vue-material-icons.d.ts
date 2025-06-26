70aa4e206272d53e22ccaa38c7433ceb6c8461b9
Luis Cabrera Benito <parzibyte@gmail.com>
2025-06-26 08:21:55 -0600

    Evitar error de tipo al usar iconos

---
+++ src/vue-material-icons.d.ts
@@ -0,0 +1,6 @@
+
+declare module 'vue-material-design-icons/*.vue' {
+  import { defineComponent } from 'vue';
+  const component: ReturnType<typeof defineComponent>;
+  export default component;
+}
