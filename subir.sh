d13f0e477fd3fdd986aa021b1e3ef9084a8eb7a1
Parzibyte <parzibyte@gmail.com>
2025-08-15 16:41:31 -0600

    Indicar usuario y servidor entre comillas

--- subir.sh
+++ subir.sh
@@ -6,10 +6,10 @@ npm run build
 workbox generateSW workbox-config.cjs
 echo "Introduce usuario@servidor:"
 read usuario_servidor
-rsync -rvnzi --delete dist/ $usuario_servidor:/var/www/misitio/apps/firmador/
+rsync -rvnzi --delete dist/ "$usuario_servidor:/var/www/misitio/apps/firmador/"
 echo "¿Subir? [s/n]"
 read opcion
 if [[ "$opcion" == "s" ]]; then
 	echo "Subiendo..."
-	rsync -rvzi --delete dist/ $usuario_servidor:/var/www/misitio/apps/firmador/
+	rsync -rvzi --delete dist/ "$usuario_servidor:/var/www/misitio/apps/firmador/"
 fi
